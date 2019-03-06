b8d303c54752716b9d167b94175fef220ce6fdb5
akash2237778 <akash2237778@gmail.com>
2019-03-06 19:28:40 +0530

    add a new place added in listview

--- app/src/main/java/com/example/markpostiononmap/MainActivity.java
+++ app/src/main/java/com/example/markpostiononmap/MainActivity.java
@@ -2,14 +2,25 @@ package com.example.markpostiononmap;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
-    ListView placesListview;
+    ListView placesListView;
+    ArrayList<String> PlacesRecordArrayList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView placesListView = (ListView)findViewById(R.id.ListStoredPlaces);
+        ArrayList<String> PlacesRecordArrayList = new ArrayList<String>();
+        PlacesRecordArrayList.add("Add new Place");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,PlacesRecordArrayList);
+        placesListView.setAdapter(arrayAdapter);
     }
 }
